7e50f81b8201b050e3d66dd5f9a04e11a5d3b0fb
wwang45 <wwang45@go.pasadena.edu>
2023-11-18 14:37:41 -0800

    Added PLayer 2

---
+++ player_2.gd
@@ -0,0 +1,50 @@
+extends Area2D
+signal hit
+
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()
+
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	var move = 1
+	if Input.is_action_pressed("move_right_2"):
+		velocity.x += move
+	if Input.is_action_pressed("move_left_2"):
+		velocity.x -= move
+	if Input.is_action_pressed("move_down_2"):
+		velocity.y += move
+	if Input.is_action_pressed("move_up_2"):
+		velocity.y -= move
+	
+	if velocity.length() > 0:
+		velocity = (velocity).normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+
+func _on_body_entered(body):
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func hide_body():
+	hide()
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos + Vector2(30, 0)
+	show()
+	$CollisionShape2D.disabled = false
